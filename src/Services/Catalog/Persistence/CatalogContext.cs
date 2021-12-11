0d62c9f281fe48294608c361158573b38a8c605f
Dilrukshan Figurado <dilrukshanf@gmail.com>
2021-12-11 23:45:48 +0530

    Category API development

---
+++ src/Services/Catalog/Persistence/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Persistence.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    internal class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetConnectionString("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetConnectionString("DatabaseSettings:ConnectionString"))
+        }
+        public IMongoCollection<Product> Products => throw new NotImplementedException();
+    }
+}
---
+++ src/Services/Catalog/Persistence/ICatalogContext.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.Persistence.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
