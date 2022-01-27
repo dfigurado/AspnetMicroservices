4f004248e9c8cdedea58d3c1ebb8b487f8f1e0b8
Dilrukshan Figurado <dilrukshanf@gmail.com>
2022-01-27 17:33:56 +0530

    Order.API development

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Ordering.API.Extentions;
+using Ordering.Infrastructure.Persistence;
 
 namespace Ordering.API
 {
@@ -7,7 +11,15 @@ namespace Ordering.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<OrderContext>((context, service) =>
+            {
+                var logger = service.GetService<ILogger<OrderContextSeed>>();
+                OrderContextSeed
+                    .SeedAsync(context, logger)
+                    .Wait();
+            });
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
